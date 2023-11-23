af089cd56b700a084d79ba4b4e4f982f7bd8d247
max cuevas <max cuevas@DESKTOP-24TMU4R>
2023-11-22 20:23:32 -0600

    Agregar archivos de proyecto.

---
+++ 6 Practica 2 U3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace _6_Practica_2_U3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(178, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(408, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Verificar si un numero es par o impar";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(51, 190);
+            label2.Name = "label2";
+            label2.Size = new Size(170, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Escribe un numero";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(276, 192);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(624, 150);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(348, 290);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 32);
+            button1.TabIndex = 4;
+            button1.Text = "Verifica";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 192, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
